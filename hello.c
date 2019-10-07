3e5b67da82fdaaf27b6f5104ecc3a19d0fb70fa9
airfix97 <anky199714@gmail.com>
2019-10-07 14:55:10 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,20 +3,14 @@
 //--------------------------------------------------------------------------------
 void say_hello(void)
 {
-  angle_1A(105); angle_1B(90); angle_1C(120);
-  _delay_ms(250);
-  angle_1C(110);
-  _delay_ms(150);
-  angle_1C(120);
-  _delay_ms(150);
-  angle_1C(110);
-  _delay_ms(150);
-  angle_1C(120);
-  _delay_ms(150);
-  angle_1C(110);
-  _delay_ms(150);
-  angle_1C(120);
-  _delay_ms(250);
+  angle_1A(105); angle_1B(80); angle_1C(130);
+  _delay_ms(700); angle_1C(110);
+  _delay_ms(700); angle_1C(130);
+  _delay_ms(700); angle_1C(110);
+  _delay_ms(700); angle_1C(130);
+  _delay_ms(700); angle_1C(110);
+  _delay_ms(700); angle_1C(130);
+  _delay_ms(700);
   angle_1A(90); angle_1B(145); angle_1C(120);
 
 }
